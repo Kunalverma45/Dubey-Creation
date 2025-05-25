81bcc8bf306fed5afae2c18844d33f5ebd480990
riyaR0831 <riyaraghuwanshi0831@gmail.com>
2025-05-25 18:33:30 +0530

    Routing part done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import Navbar from "./components/Navbar/Navbar"
+import Footer from './components/Footer/Footer';
+import LandingPage from './pages/LandingPage';
+// import other pages if needed
+// import Login from './pages/Login';
+// import ProductDetails from './pages/ProductDetails';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* Navbar is always visible */}
+      <Navbar />
+
+      {/* Only this part changes based on route */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/product/:id" element={<ProductDetails />} /> */}
+      </Routes>
+
+      {/* Footer is always visible */}
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
