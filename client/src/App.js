bccd454562e1bc8a66f60de2bc6d98e56ec98b3e
Kunal Verma <kv47871@gmail.com>
2025-05-25 20:37:25 +0530

    Adding Navbar Code again

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,20 @@
 
 // App.js
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from "./components/Navbar/Navbar"
 import Footer from './components/Footer/Footer';
 import LandingPage from './pages/LandingPage';
-// import other pages if needed
-// import Login from './pages/Login';
-// import ProductDetails from './pages/ProductDetails';
 
 const App = () => {
   return (
     <Router>
-      {/* Navbar is always visible */}
       <Navbar />
 
-      {/* Only this part changes based on route */}
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/product/:id" element={<ProductDetails />} /> */}
       </Routes>
-
-      {/* Footer is always visible */}
-      <Footer />
+      <Footer /> */}
     </Router>
   );
 };
